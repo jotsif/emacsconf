89c6dca815e18489f2b536d24c8c872adab7942c
Josef Lindman Hörnlund <josef@byax.se>
2018-01-13 16:39:45 +0100

    Emacs config

---
+++ init.el
@@ -0,0 +1,60 @@
+(require 'package)
+(setq
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                    ("org" . "http://orgmode.org/elpa/")
+                    ("melpa" . "http://melpa.org/packages/")
+                    ("melpa-stable" . "http://stable.melpa.org/packages/"))
+ package-archive-priorities '(("melpa-stable" . 1)))
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package ensime
+  :ensure t
+  :pin melpa-stable)
+
+(use-package magit
+  :ensure t)
+
+(use-package ag
+  :ensure t)
+
+(use-package projectile
+  :ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ess flx-ido ido-mode projectile magit ensime use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(windmove-default-keybindings)
+
+(use-package ido
+  :ensure t)
+
+(use-package flx-ido
+  :ensure t)
+
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
+
+(use-package ess
+  :init (require 'ess-site)
+  :ensure t)
