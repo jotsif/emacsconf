762befd5ae75a026b75d345c580c6e6442f112cd
Josef Lindman Hörnlund <jotsif@gmail.com>
2018-06-11 11:12:58 +0200

    Commit init.el update

--- init.el
+++ init.el
@@ -13,26 +13,108 @@
   (package-install 'use-package))
 (require 'use-package)
 
+(use-package ag
+  :ensure t
+  :pin melpa)
+
 (use-package ensime
   :ensure t
-  :pin melpa-stable)
+  :pin melpa-stable
+  :config
+  (setq ensime-startup-notification nil)
+  (setq ensime-startup-snapshot-notification nil)
+  (setq ensime-sem-high-face '((implicitConversion nil) (implicitParams nil))))
+
+(use-package sbt-mode
+  :ensure t
+  :pin melpa)
+
+(use-package yaml-mode
+  :ensure t
+  :pin melpa)
+
+(use-package scala-mode
+  :ensure t
+  :pin melpa)
+
+(use-package vue-html-mode
+  :ensure t
+  :pin melpa)
+
+(use-package vue-mode
+  :ensure t
+  :pin melpa)
+
+(use-package ido
+  :ensure t
+  :pin melpa
+  :config
+  (ido-mode +1)
+  (ido-everywhere +1)
+  ;; disable ido faces to see flx highlights.
+  (setq ido-enable-flex-matching t)
+  (setq ido-use-faces nil))
+
+(use-package flx-ido
+  :ensure t
+  :pin melpa
+  :config
+  (flx-ido-mode +1))
 
 (use-package magit
-  :ensure t)
-
-(use-package ag
-  :ensure t)
+  :ensure t
+  :bind (("C-x g" . magit-status)))
 
 (use-package projectile
+  :ensure t
+  :bind ("C-c p" . projectile-command-map)
+  :config
+  (projectile-global-mode +1))
+
+(use-package ess
+  :ensure t
+  :init (require 'ess-site))
+
+(use-package jedi
+  :ensure t
+  :config
+  (setq jedi:server-command '("/usr/local/bin/python3" "/Users/josef/.emacs.d/elpa/jedi-core-0.2.7/jediepcserver.py")))
+
+(use-package elpy
+  :ensure t
+  :config
+  (setq elpy-rpc-python-command "/usr/local/bin/python3"))
+
+(setq python-shell-interpreter "/usr/local/bin/python3"
+      python-shell-prompt-detect-failure-warning nil)
+
+(elpy-enable)
+
+(use-package ein
+  :pin melpa
+  :ensure t
+  :config
+  (setq ein:default-url-or-port 8888))
+
+(use-package sql
   :ensure t)
+
+(global-unset-key (kbd "C-z"))
+
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
+(setq sql-default-directory "/rutilus1.fishbrain.com:")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
  '(package-selected-packages
    (quote
-    (ess flx-ido ido-mode projectile magit ensime use-package))))
+    (elpy jedi ein vue-html-mode vue-mode ensime use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -40,21 +122,6 @@
  ;; If there is more than one, they won't work right.
  )
 
-(windmove-default-keybindings)
+(setq js-indent-level 2)
 
-(use-package ido
-  :ensure t)
-
-(use-package flx-ido
-  :ensure t)
-
-(ido-mode 1)
-(ido-everywhere 1)
-(flx-ido-mode 1)
-;; disable ido faces to see flx highlights.
-(setq ido-enable-flex-matching t)
-(setq ido-use-faces nil)
-
-(use-package ess
-  :init (require 'ess-site)
-  :ensure t)
+(put 'upcase-region 'disabled nil)
