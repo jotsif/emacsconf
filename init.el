c34ebb1c24fd7576018279676d1e8938af50802a
Josef Lindman Hörnlund <jotsif@gmail.com>
2019-03-10 10:17:27 +0100

    File as of March 2019

--- init.el
+++ init.el
@@ -17,6 +17,11 @@
   :ensure t
   :pin melpa)
 
+(use-package flycheck
+  :ensure t
+  :pin melpa)
+
+
 (use-package ensime
   :ensure t
   :pin melpa-stable
@@ -75,15 +80,10 @@
   :ensure t
   :init (require 'ess-site))
 
-(use-package jedi
-  :ensure t
-  :config
-  (setq jedi:server-command '("/usr/local/bin/python3" "/Users/josef/.emacs.d/elpa/jedi-core-0.2.7/jediepcserver.py")))
-
 (use-package elpy
   :ensure t
   :config
-  (setq elpy-rpc-python-command "/usr/local/bin/python3"))
+  (setq elpy-rpc-python-command "python3"))
 
 (setq python-shell-interpreter "/usr/local/bin/python3"
       python-shell-prompt-detect-failure-warning nil)
@@ -104,8 +104,6 @@
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
 
-(setq sql-default-directory "/rutilus1.fishbrain.com:")
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -114,13 +112,14 @@
  '(custom-enabled-themes (quote (tango-dark)))
  '(package-selected-packages
    (quote
-    (elpy jedi ein vue-html-mode vue-mode ensime use-package))))
+    (flycheck jedi ein vue-html-mode vue-mode ensime use-package))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ein:cell-input-area ((t (:background "dark cyan")))))
 
 (setq js-indent-level 2)
 
