16041bc2addeb8e062867bc33aa92a54360b8a9f
Josef Lindman Hörnlund <jotsif@gmail.com>
2020-11-06 09:59:28 +0100

    Counsel

--- init.el
+++ init.el
@@ -30,7 +30,8 @@
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(custom-enabled-themes '(tsdh-dark))
  '(ivy-mode t)
- '(package-selected-packages '(magit flycheck elpy yaml-mode ivy evil use-package)))
+ '(package-selected-packages
+   '(projectile counsel magit flycheck elpy yaml-mode ivy evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,10 +39,11 @@
  ;; If there is more than one, they won't work right.
  )
 
-(when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
 
 (ivy-mode 1)
+(use-package projectile
+  :ensure t)
+
 (use-package yaml-mode
   :ensure t
   :pin melpa)
@@ -58,8 +60,31 @@
   :init
   (elpy-enable)
   :config
-  (setq elpy-rpc-python-command "python3"))
+  (setq elpy-rpc-virtualenv-path 'current))
+(use-package company
+  :ensure t)
 
 (when (load "flycheck" t t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
+(use-package counsel
+  :ensure t)
+(setq counsel-find-file-ignore-regexp
+        (concat
+         ;; File names beginning with # or .
+         "\\(?:\\`[#.]\\)"
+         ;; File names ending with # or ~
+         "\\|\\(?:\\`.+?[#~]\\'\\)"))
+
+(use-package swiper
+  :ensure t
+  :commands swiper
+  :bind ("C-s" . counsel-grep-or-swiper)
+  :config
+  (require 'counsel)
+  (setq counsel-grep-base-command "grep -niE \"%s\" %s")
+  (setq ivy-height 20))
+
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-c g") 'magit-file-popup)
